5c860fe53d1b53728127953191c37c29294c6f13
CrimsonProtostar <5025211155@mhs.its.ac.id>
2023-11-08 14:29:12 +0700

    FIX : Header in KamusBahasa/index.blade.php

--- pbkk-1/resources/views/KamusBahasa/index.blade.php
+++ pbkk-1/resources/views/KamusBahasa/index.blade.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" data-bs-theme="dark">
-
-<!-- Basis dasar kode & layout : https://santrikoding.com/tutorial-laravel-10-4-menampilkan-data-dari-database -->
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--Bootstrap Framework-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-
-    <title>Kamus Bahasa Terbuka</title>
-
-</head>
+<x-app-layout>
 
 <body>
-    @include('layouts.navigation')
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
@@ -74,12 +59,6 @@
         </div>
 
     </div>
-
-    <!-- JQuery needed for Toastr -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-
     <script>
         //message with toastr
         @if(session() ->  has('success'))
@@ -94,6 +73,4 @@
 
         @endif
     </script>
-</body>
-
-</html>
+</x-app-layout>
