ab6c016e2a72b9a50b27b5cd9769c87f5293ec86
LuvinVictii <mdaffaharits@gmail.com>
2023-11-15 11:28:10 +0700

    test

--- pbkk-1/resources/views/KamusBahasa/create.blade.php
+++ pbkk-1/resources/views/KamusBahasa/create.blade.php
@@ -19,11 +19,12 @@
                             @csrf
 
                             <div class="form-group">
+
                                 <label class="font-weight-bold">bahasa</label>
                                 <!-- <input type="text" class="form-control @error('bahasa') is-invalid @enderror" name="bahasa" value="{{ old('bahasa') }}" placeholder=""> -->
                                 <select>
-                                    @foreach ($bahasa as $bjir)
-                                        <option value="{{ $bjir }}">{{ $bjir }}</option>
+                                    @foreach ($bahasa as $Bahasa)
+                                        <option value="{{ $Bahasa }}">{{ $Bahasa }}</option>
                                     @endforeach
                                 </select>
                                 <!-- error message untuk bahasa -->
