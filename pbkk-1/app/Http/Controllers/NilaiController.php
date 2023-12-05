9ab22b08c98120b6ff382c853e9c2a8cfb886bdd
LuvinVictii <mdaffaharits@gmail.com>
2023-12-06 01:12:24 +0700

    feat: input nilai from paket soal

--- pbkk-1/app/Http/Controllers/NilaiController.php
+++ pbkk-1/app/Http/Controllers/NilaiController.php
@@ -6,5 +6,40 @@ use Illuminate\Http\Request;
 
 class NilaiController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        // Validate the incoming request data
+        $validatedData = $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'paket_soal_id' => 'required|exists:paket_soal,id',
+            'nilai' => 'required|integer',
+        ]);
+
+        // Create a new grade entry
+        $nilais = Nilai::create($validatedData);
+
+        return response()->json(['message' => 'Nilai berhasil dimasukkan', 'nilais' => $nilais], 201);
+    }
+
+    public function update(Request $request, Grade $grade)
+    {
+        // Validate the incoming request data
+        $validatedData = $request->validate([
+            'nilais' => 'required|integer',
+        ]);
+
+        // Update the grade entry
+        $nilais->update($validatedData);
+
+        return response()->json(['message' => 'Nilai berhasil diupdate', 'nilais' => $nilais], 200);
+    }
+
+    public function destroy(Grade $grade)
+    {
+        // Delete the grade entry
+        $nilais->delete();
+
+        return response()->json(['message' => 'Nilai berhasil dihapus'], 200);
+    }
+
 }
--- pbkk-1/app/Models/Nilai.php
+++ pbkk-1/app/Models/Nilai.php
@@ -7,5 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Nilai extends Model
 {
+    protected $fillable = ['user_id', 'paket_soal_id', 'nilai'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function paketSoal()
+    {
+        return $this->belongsTo(PaketSoal::class);
+    }
     use HasFactory;
 }
